f794e644317b0bb36cf1a0a90631f19f2cd279a0
ainasabrina01 <shamsulbahri.1@buckeyemail.osu.edu>
2025-04-21 01:33:25 -0400

    for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { WelcomePage } from './components/WelcomePage'
 import { IdPage } from './components/IdPage'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/id" element={<IdPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
